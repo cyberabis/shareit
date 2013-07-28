252478d426da7f6edba5e6aca5703cf91bb355c3
cyberabis <babishek@gmail.com>
2013-07-28 16:31:28 +0530

    Editorial page updates

--- app/dao/DaoMongo.java
+++ app/dao/DaoMongo.java
@@ -10,10 +10,10 @@ public class DaoMongo {
 		
 		DB db = null;
 		try {
-			Mongo mongo = new Mongo( "localhost" , 27017 );
-			//Mongo mongo = new Mongo("ds037478.mongolab.com",37478);
-			db = mongo.getDB("shareit");
-			//db = mongo.getDB("trc");
+			//Mongo mongo = new Mongo( "localhost" , 27017 );
+			Mongo mongo = new Mongo("ds037478.mongolab.com",37478);
+			//db = mongo.getDB("shareit");
+			db = mongo.getDB("trc");
 			boolean auth = db.authenticate("trcuser", "trcpass".toCharArray());
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
